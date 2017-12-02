31e86378f117544f8e176288095b4b0ad8b962b7
dalykeith <keithdaly11@hotmail.com>
2017-12-02 14:26:46 +0000

    Where am I (location tracker)

---
+++ Where Am I/Where Am I/ViewController.swift
@@ -0,0 +1,107 @@
+//
+//  ViewController.swift
+//  Where Am I
+//
+//  Created by Keith Daly on 23/04/2016.
+//  Copyright © 2016 Thesquaredtriangle. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+import CoreLocation
+
+class ViewController: UIViewController, CLLocationManagerDelegate{
+    
+    var manager:CLLocationManager!
+    
+    
+    @IBOutlet var map: MKMapView!
+    @IBOutlet var latitudeLabel: UILabel!
+    @IBOutlet var longitudeLabel: UILabel!
+    @IBOutlet var courseLabel: UILabel!
+    @IBOutlet var speedLabel: UILabel!
+    @IBOutlet var altitudeLabel: UILabel!
+    @IBOutlet var addressLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        manager = CLLocationManager()
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
+        
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        print(locations)
+        
+        //userLocation - there is no need for casting, because we are now using CLLocation object
+        
+        let userLocation:CLLocation = locations[0]
+        
+        self.latitudeLabel.text = "\(userLocation.coordinate.latitude)"
+        
+        self.longitudeLabel.text = "\(userLocation.coordinate.longitude)"
+        
+        self.courseLabel.text = "\(userLocation.course)"
+        
+        self.speedLabel.text = "\(userLocation.speed)"
+        
+        self.altitudeLabel.text = "\(userLocation.altitude)"
+        
+        let latitude:CLLocationDegrees = userLocation.coordinate.latitude
+        
+        let longitude:CLLocationDegrees = userLocation.coordinate.longitude
+        
+        let latDelta:CLLocationDegrees = 0.005
+        
+        let lonDelta:CLLocationDegrees = 0.005
+        
+        let span:MKCoordinateSpan = MKCoordinateSpanMake(latDelta, lonDelta)
+        
+        let location:CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude, longitude)
+        
+        let region:MKCoordinateRegion = MKCoordinateRegionMake(location, span)
+        
+        map.setRegion(region, animated: false)
+        
+        
+        CLGeocoder().reverseGeocodeLocation(userLocation, completionHandler: { (placemarks, error) -> Void in
+            
+            if (error != nil) {
+                
+                print(error)
+                
+            } else {
+                
+                if let p = placemarks?[0] {
+                    
+                    var subThoroughfare:String = ""
+                    
+                    if (p.subThoroughfare != nil) {
+                        
+                        subThoroughfare = p.subThoroughfare!
+                        
+                    }
+                    
+                    self.addressLabel.text = "\(subThoroughfare) \(p.thoroughfare) \n \(p.subLocality) \n \(p.subAdministrativeArea) \n \(p.postalCode) \n \(p.country)"
+                }
+                
+                
+            }
+            
+        })
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
